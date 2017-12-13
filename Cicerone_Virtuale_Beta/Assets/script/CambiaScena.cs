c11b2c4472ecf0ee076e0141c2af01ea153780ff
HeavyRainLQ <lucaquercetti@gmail.com>
2017-12-13 16:28:21 +0100

    Inserite scene e agg script

---
+++ Cicerone_Virtuale_Beta/Assets/script/CambiaScena.cs
@@ -0,0 +1,37 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Vuforia;
+
+public class CambiaScena : MonoBehaviour
+{
+
+
+    // Use this for initialization
+    void Start()
+    {
+        VuforiaBehaviour.Instance.enabled = false;
+
+    }
+    public void caricaScenaVuforia()
+    {
+        if (PlayerPrefs.GetInt("isFirstTime") != 1)
+        {
+            SceneManager.LoadScene("Intro");
+            PlayerPrefs.SetInt("isFirstTime", 1);
+            PlayerPrefs.Save();
+
+        }
+        else
+        {
+            SceneManager.LoadScene("VuforiaScene");
+
+        }
+        //SceneManager.LoadScene("prova vuforia");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Cicerone_Virtuale_Beta/Assets/script/toglistat.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class toglistat : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        PlayerPrefs.SetInt("isFirstTime", 0);
+        PlayerPrefs.Save();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
