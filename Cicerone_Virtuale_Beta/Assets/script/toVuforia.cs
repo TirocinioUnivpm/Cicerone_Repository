373997a59248d77f8c84093070ebc4d6e410e3ef
HeavyRainLQ <lucaquercetti@gmail.com>
2017-12-13 15:53:03 +0100

    Caricamento scena iniziale

---
+++ Cicerone_Virtuale_Beta/Assets/script/toVuforia.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Vuforia;
+
+public class toVuforia : MonoBehaviour {
+    
+
+	// Use this for initialization
+	void Start () {
+        VuforiaBehaviour.Instance.enabled = false;
+
+    }
+	public void caricaScenaVuforia(string prova_vuforia)
+    {
+        
+        SceneManager.LoadScene(prova_vuforia);
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
