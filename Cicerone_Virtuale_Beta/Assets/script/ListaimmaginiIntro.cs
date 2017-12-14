013bb39d3c74ea4314e4b0ad4975072088c5924f
HeavyRainLQ <lucaquercetti@gmail.com>
2017-12-14 19:04:37 +0100

    slide bar per intro con immagini di prova

---
+++ Cicerone_Virtuale_Beta/Assets/script/ListaimmaginiIntro.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+
+public class ListaimmaginiIntro : MonoBehaviour {
+    Image myImageComponent;
+    public Sprite myFirstImage; //Drag your first sprite here in inspector.
+    public Sprite mySecondImage; //Drag your second sprite here in inspector.
+    public Sprite myThirdImage;
+  
+    void Start() //Lets start by getting a reference to our image component.
+    {
+        myImageComponent = GetComponent<Image>(); //Our image component is the one attached to this gameObject.
+        
+       
+    }
+
+    public void SetImage(float i) //method to set our first image
+    {
+        if (i == 0)
+        {
+            myImageComponent.sprite = myFirstImage;
+
+        }else if (i == 1)
+        {
+            myImageComponent.sprite = mySecondImage;
+        }
+        else
+        {
+            myImageComponent.sprite = myThirdImage;
+        }
+        
+    }
+
+   
+
+}
