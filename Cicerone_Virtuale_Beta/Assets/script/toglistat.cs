d5d6e2ada90902bae7365815be60908759dbcf6e
HeavyRainLQ <lucaquercetti@gmail.com>
2017-12-14 15:52:09 +0100

    le bestemmie

--- Cicerone_Virtuale_Beta/Assets/script/toglistat.cs
+++ Cicerone_Virtuale_Beta/Assets/script/toglistat.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Vuforia;
 
 public class toglistat : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+        VuforiaBehaviour.Instance.enabled = true;
         PlayerPrefs.SetInt("isFirstTime", 0);
         PlayerPrefs.Save();
 
