94c4f4b5a7d89ee832959ac5af2d7276c748ed79
HeavyRainLQ <lucaquercetti@gmail.com>
2017-12-15 12:57:54 +0100

    vignetta

---
+++ Cicerone_Virtuale_Beta/Assets/script/ProvaBottoneSfera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ProvaBottoneSfera : MonoBehaviour {
+    Image prova;
+    public Sprite spriteprova;
+	// Use this for initialization
+    void Start()
+    {
+        prova = GetComponent<Image>();
+    }
+	public void provaBottone()
+        
+    {
+        prova.sprite= spriteprova;
+        Debug.Log("ho cliccato");
+    }
+	
+	
+}
---
+++ Cicerone_Virtuale_Beta/Assets/script/Sfera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sfera : MonoBehaviour {
+    public GameObject text;
+    // Use this for initialization
+    void OnMouseDown()
+    {
+        if (text.activeSelf)
+        {
+            text.SetActive(false);
+
+        }
+        else {
+
+            text.SetActive(true);
+
+        }
+       
+        
+    }
+}
