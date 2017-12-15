9d442a407a38e539a54b4a5512a4d99e4295a7c2
HeavyRainLQ <lucaquercetti@gmail.com>
2017-12-15 15:09:50 +0100

    lol

---
+++ Cicerone_Virtuale_Beta/Assets/script/ScriptCamera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptCamera : MonoBehaviour {
+    public bool on;
+    public Camera cameraVuforia;
+    public Camera cameraScroll;
+
+    // Use this for initialization
+    void OnMouseDown(){
+
+        if (on)
+        {
+            cameraVuforia.enabled = false;
+            cameraScroll.enabled = true;
+            on = false;
+        }
+        else
+        {
+            cameraVuforia.enabled = true;
+            cameraScroll.enabled = false;
+            on = true;
+        }
+		
+	}
+	
+	// Update is called once per frame
+	
+}
