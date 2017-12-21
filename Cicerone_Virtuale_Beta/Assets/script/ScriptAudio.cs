1819e065ba34d178be81f0bdce20034006fae30f
HeavyRainLQ <lucaquercetti@gmail.com>
2017-12-21 14:24:27 +0100

    aggiornamento effetti e audio

--- Cicerone_Virtuale_Beta/Assets/script/ScriptAudio.cs
+++ Cicerone_Virtuale_Beta/Assets/script/ScriptAudio.cs
@@ -4,16 +4,44 @@ using UnityEngine;
 
 
 public class ScriptAudio : MonoBehaviour {
-
+    public Camera camera;
     public AudioSource mp3;
     public GameObject sfera;
-    
-
+    public GameObject animazione;
     public bool attivo;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
     // Use this for initialization
     public void Start()
     {
-        attivo = true;
+        
+    }
+
+    public void Update()
+    {
+        float distanza = (camera.transform.position.z - sfera.transform.position.z);
+
+        if (distanza < 0){
+
+            distanza = -distanza;
+
+            }else if (distanza < 20){
+        
+                mp3.Play();
+                attivo = true;
+                sfera.GetComponent<Renderer>().material.color = Color.yellow;
+                animazione.SetActive(false);
+
+
+                }else{
+        
+                    Debug.Log("sei troppo lontano");
+           
+                }
     }
     void OnMouseDown()
 
@@ -22,13 +50,15 @@ public class ScriptAudio : MonoBehaviour {
         {
             mp3.Play();
             attivo = true;
-            sfera.GetComponent<Renderer>().material.color= Color.red;
+            sfera.GetComponent<Renderer>().material.color= Color.yellow;
+            animazione.SetActive(false);
         }
         else
         {
             mp3.Pause();
             attivo = false;
             sfera.GetComponent<Renderer>().material.color = Color.white;
+            animazione.SetActive(true);
         }
     }
 }
--- Cicerone_Virtuale_Beta/Assets/script/Sfera.cs
+++ Cicerone_Virtuale_Beta/Assets/script/Sfera.cs
@@ -4,18 +4,25 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Sfera : MonoBehaviour {
-    public GameObject text;
+    public GameObject pannello;
+    public GameObject animazione;
+    public GameObject sfera;
+
     // Use this for initialization
     void OnMouseDown()
     {
-        if (text.activeSelf)
+        if (pannello.activeSelf)
         {
-            text.SetActive(false);
-
+            sfera.GetComponent<Renderer>().material.color = Color.yellow;
+            pannello.SetActive(false);
+            animazione.SetActive(false);
         }
         else {
 
-            text.SetActive(true);
+            sfera.GetComponent<Renderer>().material.color = Color.white;
+            pannello.SetActive(true);
+            animazione.SetActive(true);
+
 
         }
        
